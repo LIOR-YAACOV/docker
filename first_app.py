cd99d71c3e78442ec9a1a6d6030abb277387e4b8
LIOR-YAACOV <LIORBE111110@GMAIL.COM>
2025-11-11 10:57:31 +0200

    first app files added

--- first_app.py
+++ first_app.py
@@ -21,4 +21,4 @@ def login(name):
     # return f'Hello, {name}! You have successfully logged in.'
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80)
+    app.run(host='0.0.0.0', port=80)
